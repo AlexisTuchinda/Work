c6d7d90037646fbd1f8abcbfb88ed0353bac6eab
Alexis Tuchinda <50222834+AlexisTuchinda@users.noreply.github.com>
2021-01-28 18:47:14 -0800

    Trial with separate order linked list

--- Sushi_Data/main.js
+++ Sushi_Data/main.js
@@ -1,7 +1,8 @@
-//Node
+//Node for Plate
 class Piece{
     constructor (filling){
         this.filling = filling;
+        this.appeal = Math.floor(Math.random()*10)+1;
         this.before = null; //prev in Linked List
         this.behind = null; //next in Linked List
     }
@@ -51,7 +52,7 @@ class Plate{
 
 }
 
-//Linked List (for sorting equal data)
+//Linked List for Plate
 class Roll{
     constructor (){
         this.sushi = null; //head of linked list
@@ -65,20 +66,92 @@ class Roll{
         }
         //once found the end of the linked list
         current.behind = new Piece(filling);
+        current.behind.before = current;
     }
 
-    eatPiece(current, filling){
-        if (this.sushi.filling == filling){
+    eatPiece(current, appeal){
+        if (this.sushi.appeal == appeal){
             this.sushi = this.sushi.behind;
             this.sushi.behind.before = null;
             return this.sushi;
         }
 
-        while (current.behind.filling != filling){ //however, the filling within the Linked List are all going to be the same. Therefore, the head sushi will be deleted.
+        while (current.behind.appeal != appeal){ //however, the filling within the Linked List are all going to be the same. Therefore, the head sushi will be deleted.
             current = current.behind; //traversing through Linked List
         }
         current.behind = current.behind.behind; //changing pointers
         current.behind.behind.before = current;
         return current;
+    }
+}
+
+class Order{//node for Kitchen
+    constructor (value){
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+//stack Linked List
+class Kitchen{
+    constructor(){
+        this.cooking = null; //head
+    }
+
+    addOrder(current, value){ //insert
+        if (this.cooking == null){
+            this.cooking = new Order(value);
+            return this.cooking;
+        }else{
+            while (current.next!= null){
+                current = current.next; //going through list to the end
+            }
+            current.next = new Order(value);
+            current.next.prev = current;
+            return current;
+        }
+    }
+
+    deleteOrder(current, value){
+        if (current.next.value == value){
+            current.next = current.next.next;
+            current.next.next.prev = current;
+        }else{
+            return this.deleteOrder(current.next, value)
+        }
+        return current;
+    }
+}
+
+
+class Waiter{
+    constructor(){
+        this.plate = new Plate();
+        this.order = "";
+        this.kitchen = new Kitchen();
+    }
+
+    handleChange(event) {
+        this.order = event.target.value;
+    }   
+
+    createOrder(event){ //handleSubmit
+        this.kitchen.addOrder(this.kitchen.cooking, event.target.value);
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <form onSubmit = {this.createOrder}>
+                        <label>
+                            <input type = "text" value = {this.order} placeholder = {"Place order."} onChange = {this.handleChange}/>
+                        </label>
+                        <input type = "submit" value = "Submit"/>
+                    </form>
+                </div>
+            </div>
+        )
     }
 }
